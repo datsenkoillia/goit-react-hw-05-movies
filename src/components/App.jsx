7358306d267f56e8b7d5a5584f21ec1f31365594
Illia Datsenko <datsenko.i@gmail.com>
2023-05-28 11:24:54 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,8 +20,8 @@ const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path="*" element={<HomePage />} />
         </Route>
-        <Route path="*" element={<HomePage />}></Route>
       </Routes>
     </div>
   );
